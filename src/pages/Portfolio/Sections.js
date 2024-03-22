41a242ce82d50dd7dcc60c086a80aff342f3cb48
kmkinney <kevinmk712@gmail.com>
2024-03-22 17:55:10 -0600

    Purging old portfolio stuff

--- src/pages/Portfolio/Sections.js
+++ src/pages/Portfolio/Sections.js
@@ -1,228 +1,39 @@
 // Sections for the Portfolio
 const yaLink = 'https://docs.google.com/document/d/1A0UTUjZNTFxWHWP_EkvAAXWOUsasWZxxhh-4GIHG5SU/edit';
 const friendBuddyLink = 'https://docs.google.com/document/d/1rwR8CX6AnInUqD0EnNOCj7jP_0qlPjM3_Lu-EqB3qL0/edit';
-const friendEraserLink = 'https://docs.google.com/document/d/135gUYGC5yrX9uzYOqwSuKjcCYs2FwmsDs92lOF8EGFo/edit'; 
+const friendEraserLink = 'https://docs.google.com/document/d/135gUYGC5yrX9uzYOqwSuKjcCYs2FwmsDs92lOF8EGFo/edit';
 const fsyLink = 'https://docs.google.com/document/d/1IFyIQnddGzfhYmDhZnsZtU6yGEkgKoufy0gdGG1SBjk/edit';
 const gospelLink = 'https://docs.google.com/document/d/1z7tHTxDhaD0EKxjMwtnocAmwA2NW8Hrzh4geCKd-9Rk/edit';
 const criterionLink = 'https://docs.google.com/document/d/1pAXzvXE36BKCqxHXl_3Z0ArAablRLGClbJ45amk24lU/edit';
 const foolLoveLink = 'https://docs.google.com/document/d/1ULomeWj2cXAMBNvf3qwACvVgKSWAb0Gk/edit';
 
 export const Sections = [
-    {
-        name: '',
+  {
+    name: '',
+    items: [
+      {
+        title: 'Résumé',
+        italicized: false,
+        tag: 'resume',
         items: [
-            {
-                title: 'Résumé',
-                italicized: false,
-                tag: 'resume',
-                items: [
-                    {
-                        title: 'Résumé',
-                        description: '',
-                        src: '/assets/GKinney_Resume.pdf',
-                        link: '/assets/GKinney_Resume.pdf',
-                        linktext: 'View Fullscreen'
-                    }
-                ]
-            },
+          {
+            title: 'Résumé',
+            description: '',
+            src: '/assets/GKinney_Resume.pdf',
+            link: '/assets/GKinney_Resume.pdf',
+            linktext: 'View Fullscreen'
+          }
         ]
-    },
-    {
-        name: 'Writing Samples',
-        tag: '',
-        items: [
-            {
-                title: 'Friend',
-                italicized: true,
-                description: 'Sample Articles',
-                tag: 'friend',
-                items: [
-                    {
-                        title: 'Cover Letter',
-                        description: '',
-                        src: '/assets/Kinney_CoverLetter.pdf',
-                        link: '/assets/Kinney_CoverLetter.pdf',
-                        linktext: 'View Fullscreen'
-                    },
-                    {
-                        title: '"Ministering Buddy"',
-                        description: 'Elizabeth helps her mom minister to Sister Monroe, a blind woman. This story is special to me because the real life Sister Monroe is still my very good friend.',
-                        src: '/assets/Ministering-Buddy.pdf',
-                        link: '/assets/Ministering-Buddy.pdf',
-                        linktext: 'View Fullscreen'
-                    },
-                    {
-                        title: '"The Eraser"',
-                        description: 'This story follows Ellie as she learns about the importance of honesty and repentance. This story is based on an experience I had in elementary school.',
-                        src: '/assets/The-Eraser.pdf',
-                        link: '/assets/The-Eraser.pdf',
-                        linktext: 'View Fullscreen'
-                    }
-                ]
-            },
-            {
-                title: 'YA Weekly',
-                italicized: true,
-                description: 'Writing Samples',
-                tag: 'ya',
-                items: [
-                    {
-                        title: 'Cover Letter',
-                        description: '',
-                        src: '/assets/YA_Weekly_Cover_Letter.pdf',
-                        link: '/assets/YA_Weekly_Cover_Letter.pdf',
-                        linktext: 'View Fullscreen'
-                    },
-                    {
-                        title: '"Combating Toxic Perfectionism"',
-                        description: 'This article shares my experience dealing with perfectionism. I talk about how remembering my identity as a child of God helped me recognize my intrinsic value and combat thoughts of inadequacy.',
-                        src: '/assets/Combating_Toxic_Perfectionism_YA_Weekly.pdf',
-                        link: '/assets/Combating_Toxic_Perfectionism_YA_Weekly.pdf',
-                        linktext: 'View Fullscreen'
-                    }
-                ]
-            },
-            {
-                title: 'For the Strength of Youth',
-                italicized: true,
-                description: 'Writing Samples',
-                tag: 'fsy',
-                items: [
-                    {
-                        title: 'Cover Letter',
-                        description: '',
-                        src: '/assets/FSYKinney_CoverLetter.pdf',
-                        link: '/assets/FSYKinney_CoverLetter.pdf',
-                        linktext: 'View Fullscreen'
-                    },
-                    {
-                        title: '"Eight Tips for a More Meaningful Scripture Study"',
-                        description: 'This article gives ideas for improving scripture study. Each idea includes a quote from a general authority and a brief description. These ideas came from personal experience and suggestions from friends and family.',
-                        src: '/assets/FSY-article.pdf',
-                        link: '/assets/FSY-article.pdf',
-                        linktext: 'View Fullscreen'
-                    }
-                ]
-            },
-            {
-                title: 'Gospel Living',
-                italicized: false,
-                description: 'Writing Samples',
-                tag: 'gl',
-                items: [
-                    {
-                        title: 'Cover Letter',
-                        description: '',
-                        src: '/assets/Gospel-Living-Cover-Letter.pdf',
-                        link: '/assets/Gospel-Living-Cover-Letter.pdf',
-                        linktext: 'View Fullscreen'
-                    },
-                    {
-                        title: '100 Word Summaries',
-                        description: 'For these shorter articles, I condensed a general conference talk, a section from gospel topics, and a section from the <em>For the Strength of Youth</em> booklet.',
-                        src: '/assets/Gospel-living-100-Word-Summaries.pdf',
-                        link: '/assets/Gospel-living-100-Word-Summaries.pdf',
-                        linktext: 'View Fullscreen'
-                    }
-                ]
-            },
-            {
-                title: '100-Word Teaser',
-                italicized: false,
-                description: '',
-                tag: 'tease',
-                items: [
-                    {
-                        title: 'Growth Mindset Teaser',
-                        description: 'This teaser is for the article “Helping Children and Youth Develop a Growth Mindset” from the February 2022 <em>Liahona</em>. ',
-                        src: '/assets/Article-Teaser-Growth-Mindset.pdf',
-                        link: 'assets/Article-Teaser-Growth-Mindset.pdf',
-                        linktext: 'View Fullscreen'
-                    },
-                ]
-            }
-        ]
-    },
-    {
-        name: 'Editing Samples',
-        items: [
-            {
-                title: 'Substantive Editing',
-                tag: 'sub',
-                items: [
-                    {
-                        title: '<em>YA Weekly</em> Draft',
-                        description: 'In this Google Doc, I explain the differences between my first draft and my final draft of “Combating Toxic Perfectionism” and talk about why I made certain changes. This is the topic I was most excited to write about, and the article I spent the most time editing. I mostly edited for flow and content. I think overall my edits improved my message, but I want to revisit this article again and try to make it even better.',
-                        link: yaLink,
-                        linktext: 'Click here to view Google Doc'
-                    },
-                    {
-                        title: '<em>Friend</em> Article, “Ministering Buddy” Draft',
-                        description: 'In this Google Doc, I explain the differences between my first draft and my final draft of “Ministering Buddy” and talk about why I made certain changes. This article only went through minor changes.',
-                        link: friendBuddyLink,
-                        linktext: 'Click here to view Google Doc'
-                    },
-                    {
-                        title: '<em>Friend</em> Article, “The Eraser” Draft',
-                        description: 'In this Google Doc, I explain the differences between my first draft and my final draft of “The Eraser” and talk about why I made certain changes. This article went through more edits than “Ministering Buddy.” I edit mostly for organization and plot.',
-                        link: friendEraserLink,
-                        linktext: 'Click here to view Google Doc'
-                    }
-                    ,
-                    {
-                        title: '<em>For the Strength of Youth</em> Draft',
-                        description: 'This article gives ideas for improving scripture study. Each idea includes a quote from a general authority and a brief description. These ideas came from personal experience and suggestions from friends and family.',
-                        link: fsyLink,
-                        linktext: 'Click here to view Google Doc'
-                    },
-                    {
-                        title: 'Gospel Living Draft',
-                        description: 'In this Google Doc, I explain the differences between my first drafts and my final drafts of my 100-word articles, and why I made certain changes. The content of the articles does not change much, but I edit for wordiness and tone.',
-                        link: gospelLink,
-                        linktext: 'Click here to view Google Doc'
-                    },
-                    {
-                        title: '<em>Criterion</em> Paper',
-                        description: 'I edited this article, “Contemporary Existentialism in Orwell\'s Novels Keep The Aspidistra Flying and The Clergyman\'s Daughter,” my first semester at <em>Criterion</em>. This was my first experience editing literary criticism, and I learned a lot!  This semester, I have the opportunity to be a lead copyeditor for <em>Criterion</em>, so I get to edit even more papers! I also wrote a fun summary of my experience editing this paper, which you can find under the “Copyediting” section.',
-                        link: criterionLink,
-                        linktext: 'Click here to view Google Doc'
-                    }
-                ]
-            },
-            {
-                title: 'Copyediting',
-                tag: 'copy',
-                items: [
-                    {
-                        title: 'A Fool in Love',
-                        description: 'This document is just straight-up copyediting. It was the final exam for Modern American Usage. I have included the original assignment and my final product. (You can also view track changes to see my process.) I think I missed two items on this exam: I did not notice a character\'s name changed spelling, and I forgot a comma.',
-                        link: foolLoveLink,
-                        linktext: 'Click here to view Google Doc'
-                    },
-                    {
-                        title: 'Experience Editing for <em>Criterion</em>',
-                        description: 'This is a summary of my first semester editing for <em>Criterion</em>. At first, I felt pretty overwhelmed because I was not familiar with the topic of my paper, but I did some research and ended up really enjoying my experience. I talk about some of my favorite copyediting changes.',
-                        src: '/assets/Summary-of-Experience-Editing-for-Criterion.pdf',
-                        link: '/assets/Summary-of-Experience-Editing-for-Criterion.pdf',
-                        linktext: 'View Fullscreen'
-                    }
-
-                ]
-            },
-            {
-                title: 'General Publications',
-                tag: 'gp',
-                items: [
-                    {
-                        title: 'Long Editing Test',
-                        description: '',
-                        src: '/assets/General-Publications-Long-Editing-Test.pdf',
-                        link: '/assets/General-Publications-Long-Editing-Test.pdf',
-                        linktext: 'View Fullscreen'
-                    }
-
-                ]
-            }
-        ]
-    }
+      },
+    ]
+  },
+  {
+    name: 'Writing Samples',
+    tag: '',
+    items: []
+  },
+  {
+    name: 'Editing Samples',
+    items: []
+  }
 ]
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,107 +1,74 @@
 import './Projects.css'
 
 export default function Projects() {
-    /* Images */
-    const bookCover = require('../../images/book-cover.png');
-    const bookStack = require('../../images/book-stack.jpg');
-    const bookRing = require('../../images/ring.jpg');
-    const adventureHome = require('../../images/adventures.png');
-    const adventurePage = require('../../images/adventure2.png');
+  /* Images */
+  const bookCover = require('../../images/book-cover.png');
+  const bookStack = require('../../images/book-stack.jpg');
+  const bookRing = require('../../images/ring.jpg');
+  const adventureHome = require('../../images/adventures.png');
+  const adventurePage = require('../../images/adventure2.png');
 
-    /* PDFs */
-    const root = process.env.PUBLIC_URL;
-    const norton = root + '/assets/Norton-Pitch.pdf';
-    const cloudNine = root + '/assets/Cloud9.pdf';
-    const criterion = root + '/assets/Summary-of-Experience-Editing-for-Criterion.pdf';
-    const chemistry = root + '/assets/Chemistry-of-Literary-Theory.pdf';
+  /* PDFs */
+  const root = process.env.PUBLIC_URL;
+  const norton = root + '/assets/Norton-Pitch.pdf';
+  const cloudNine = root + '/assets/Cloud9.pdf';
+  const criterion = root + '/assets/Summary-of-Experience-Editing-for-Criterion.pdf';
+  const chemistry = root + '/assets/Chemistry-of-Literary-Theory.pdf';
 
-    return (
-        <div className="page container-fluid bg-light text-primary">
-            <div className="row justify-content-around">
-                <div className="col-12 my-lg-2">
-                    <h1>Just-for-Fun Projects!</h1>
-                </div>
-
-                <div className="col-12 bg-secondary text-light">
-                    <h3>Academic</h3>
-                </div>
-                <div className="col-12">
-                    <a className="article-link" href={norton} target="_blank">
-                        <h3>Norton Pitch<i className="bi bi-download ml-2"></i></h3>
-                    </a>
-                    <p>A pretend pitch to the<em> Norton Anthology </em>
-                        arguing for the inclusion of Salman Rushdie's
-                        <em> Midnight's Children </em>
-                        in the <em>Modernist Norton.</em>
-                    </p>
-                </div>
-
-                <div className="col-12">
-                    <a className="article-link" href={chemistry} target="_blank">
-                        <h3>Chemistry of Literary Theory<i className="bi bi-download ml-2"></i></h3>
-                    </a>
-                    <p>
-                        A creative project comparing eight literary theoretical lenses to eight chemistry concepts.
-                    </p>
-                </div>
-
-                <div className="col-12">
-                    <a className="article-link" href={criterion} target="_blank">
-                        <h3>Summary of Editing Experience in Criterion<i className="bi bi-download ml-2"></i></h3>
-                    </a>
-                    <p>
-                        A summary of my experience editing for the BYU journal Criterion for the first time.                </p>
-                </div>
-
-                <div className="col-12 bg-secondary text-light mb-2">
-                    <h3>Creative</h3>
-                </div>
-                <div className="col-12">
-                    <h3><em>Cloud-9</em> Book</h3>
-                    <p>
-                        <em>A creative non-fiction book about how my husband and I met and fell in love.</em>
-                    </p>
-                </div>
-                <div className="col-4 text-center align-self-center">
-                    <a className="book-link" href={cloudNine} target="_blank">
-                        <img className="img w-50" src={bookCover} alt=""/>
-                            <br/>
-                                <h4>Click to view</h4>
-                            </a>
-                        </div>
-                        <div className="col-4">
-                            <img className="img-fluid" src={bookStack} alt="" />
-                        </div>
-                        <div className="col-4">
-                            <img className="img-fluid" src={bookRing} alt="" />
-                        </div>
-
-                        <div className="col-12">
-                            <div className="divider bg-secondary"></div>
-                        </div>
-
-                        <div className="col-12">
-                            <h3><em>Literary Misadventures</em></h3>
-                            <p><em>A text-based Choose-Your-Own-Adventure story based on the Divine Comedy, Bisclavret, Laustic,
-                                Lanval, Don Quixote, Antigone and the Ramayana.</em></p>
-                        </div>
-                        <div className="col-5">
-                            <img className="img-fluid" src={adventureHome} alt="" />
-                        </div>
-                        <div className="col-2 text-center align-self-center">
-                            <a className="book-link" href="https://adventure.gretchenpickles.com/">
-                                <h4>Click to play</h4>
-                            </a>
-                        </div>
-                        <div className="col-5">
-                            <img className="img-fluid" src={adventurePage} alt="" />
-                        </div>
-                        <div className='col-12 text-center text-secondary my-5'>
-                            <h2>More projects coming soon...</h2>
-                        </div>
-                </div>
+  return (
+    <div className="page container-fluid bg-light text-primary">
+      <div className="row justify-content-around">
+        <div className="col-12 my-lg-2">
+          <h1>Just-for-Fun Projects!</h1>
+        </div>
 
 
-            </div>
-    );
+        <div className="col-12">
+          <h3><em>Cloud-9</em> Book</h3>
+          <p>
+            <em>A creative non-fiction book about how my husband and I met and fell in love.</em>
+          </p>
+        </div>
+        <div className="col-4 text-center align-self-center">
+          <a className="book-link" href={cloudNine} target="_blank">
+            <img className="img w-50" src={bookCover} alt="" />
+            <br />
+            <h4>Click to view</h4>
+          </a>
+        </div>
+        <div className="col-4">
+          <img className="img-fluid" src={bookStack} alt="" />
+        </div>
+        <div className="col-4">
+          <img className="img-fluid" src={bookRing} alt="" />
+        </div>
+
+        <div className="col-12">
+          <div className="divider bg-secondary"></div>
+        </div>
+
+        <div className="col-12">
+          <h3><em>Literary Misadventures</em></h3>
+          <p><em>A text-based Choose-Your-Own-Adventure story based on the Divine Comedy, Bisclavret, Laustic,
+            Lanval, Don Quixote, Antigone and the Ramayana.</em></p>
+        </div>
+        <div className="col-5">
+          <img className="img-fluid" src={adventureHome} alt="" />
+        </div>
+        <div className="col-2 text-center align-self-center">
+          <a className="book-link" href="https://kmkinney.github.io/gretchen-text-adventure/" target='_blank'>
+            <h4>Click to play</h4>
+          </a>
+        </div>
+        <div className="col-5">
+          <img className="img-fluid" src={adventurePage} alt="" />
+        </div>
+        <div className='col-12 text-center text-secondary my-5'>
+          <h2>More projects coming soon...</h2>
+        </div>
+      </div>
+
+
+    </div>
+  );
 }
