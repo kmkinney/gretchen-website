c04e2715b0dc2c61a29b093acce95feb2828719f
kmkinney <kevinmk712@gmail.com>
2024-03-22 18:40:16 -0600

    Last fixes

--- src/pages/Home/AboutContent.js
+++ src/pages/Home/AboutContent.js
@@ -1,20 +1,19 @@
 export const AboutContent = {
-    about: `Gretchen is an avid reader who loves puzzles, 
-    animals, and ice cream. She is currently a BYU student majoring 
-    in English and minoring in editing and French. She plans to graduate 
-    April 2024.`,
-    skills: [
-        "Experience with substantive editing and copyediting",
-        "Proficient in Adobe InDesign, Photoshop, and Illustrator",
-        "Advanced French speaking, reading, and writing",
-        "Polished creative, academic, and professional writing"
-    ],
-    interests: [
-        "Reading (especially fantasy!)",
-        "Creative writing",
-        "Puzzles",
-        "Petting dogs",
-        "Hiking"
-    ]
+  about: `I’m an editor who loves reading, puzzles, and dogs. My major is English and my minors are editing and French. I graduate from Brigham Young University with a Bachelor's of Arts in April 2024. `,
+  skills: [
+    "Manuscript evaluation and acquisition",
+    "Well-versed in grammar, usage, and punctuation principles from The Chicago Manual of Style",
+    "Proficiency in MS Outlook, Teams, and Word",
+    "Proficiency in Adobe InDesign and Acrobat",
+    "Advanced French speaking, reading, and writing",
+    "Knowledge of MLA, APA, and Chicago citation styles",
+  ],
+  interests: [
+    "Reading 100+ books per year (my favorite fiction genres are suspense, romance, and fantasy)",
+    "Writing satirical short stories and book reviews",
+    "Watching and reading true crime (not after 10pm)",
+    "Playing social deduction games with friends (may be enemies post-game)",
+    "Taking my dog to the park"
+  ]
 
 }
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -4,70 +4,69 @@ import { Sections } from './Sections';
 import BottomBar from '../../components/BottomBar/BottomBar';
 
 export default function Portfolio() {
-    // const NortonPDF = process.env.PUBLIC_URL+'/assets/Norton-Pitch.pdf#toolbar=0';
-    const NortonPDF = process.env.PUBLIC_URL + '/assets/Norton-Pitch.pdf#view=fitH';
-    const NortonTitle = 'Norton Pitch'
-    // const NortonPDF = process.env.PUBLIC_URL+'/assets/Norton-Pitch.pdf';
+  // const NortonPDF = process.env.PUBLIC_URL+'/assets/Norton-Pitch.pdf#toolbar=0';
+  const NortonPDF = process.env.PUBLIC_URL + '/assets/Norton-Pitch.pdf#view=fitH';
+  const NortonTitle = 'Norton Pitch'
+  // const NortonPDF = process.env.PUBLIC_URL+'/assets/Norton-Pitch.pdf';
 
-    return (
-        <div className="page text-primary">
-            {/* <Sidebar sections={Sections}/> */}
-            <h1 className='text-center my-2'>Portfolio</h1>
-            <SectionNav />
-            <SectionContent />
+  return (
+    <div className="page text-primary">
+      {/* <Sidebar sections={Sections}/> */}
+      <h1 className='text-center my-2'>Portfolio</h1>
+      <SectionNav />
+      <SectionContent />
 
-            {/* <BottomBar to='#top' sections={Sections}>
+      {/* <BottomBar to='#top' sections={Sections}>
             </BottomBar> */}
-            <a className='back-to-top' href='#top'>Back to Top</a>
-        </div>
-    );
+      <a className='back-to-top' href='#top'>Back to Top</a>
+    </div>
+  );
 }
 
 function SectionContent() {
-    const allSectionItems = [];
-    for (let section of Sections) {
-        for (let item of section.items)
-            allSectionItems.push(item)
-    }
-    return (
-        <div>
-            {allSectionItems.map((item, index) => {
-                return (
-                    <PortfolioSection key={index} item={item} />
-                )
-            })
-            }
-        </div>
-    )
+  const allSectionItems = [];
+  for (let section of Sections) {
+    for (let item of section.items)
+      allSectionItems.push(item)
+  }
+  return (
+    <div>
+      {allSectionItems.map((item, index) => {
+        return (
+          <PortfolioSection key={index} item={item} />
+        )
+      })
+      }
+    </div>
+  )
 }
 
 function SectionNav() {
-    return (
-        <div>
-            {Sections.map((sec, index) => {
-                return (
-                    <SectionLinks key={index} name={sec.name} items={sec.items} />
-                )
-            })}
-        </div>
-    )
+  return (
+    <div>
+      {Sections.map((sec, index) => {
+        return (
+          <SectionLinks key={index} name={sec.name} items={sec.items} />
+        )
+      })}
+    </div>
+  )
 }
 
 function SectionLinks(props) {
-    const sectionName = props.name;
-    const sectionItems = props.items;
-    return (
-        <div>
-            <h2>{sectionName}</h2>
-            <div className='section-buttons'>
-                {sectionItems.map((item, index) => {
-                    return (
-                        <a key={index} className={item.italicized ? 'section-button em' : 'section-button'}
-                            href={'#' + item.tag}>{item.title}</a>
-                    )
-                })
-                }
-            </div>
-        </div>
-    )
+  const sectionName = props.name;
+  const sectionItems = props.items;
+  return (
+    <div>
+      <div className='section-buttons'>
+        {sectionItems.map((item, index) => {
+          return (
+            <a key={index} className={item.italicized ? 'section-button em' : 'section-button'}
+              href={'#' + item.tag}>{item.title}</a>
+          )
+        })
+        }
+      </div>
+    </div>
+  )
 }
